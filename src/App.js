0b6cf52f3439fb2aac5283a9c9e60e1a1e4ac2d4
Iris2030 <ibazhinova@gmail.com>
2022-04-27 19:25:07 +0200

    hw done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import './App.css';
 import Form from './Components/Form/Form';
@@ -8,28 +8,23 @@ import Container from './Components/Container/Container';
 import s from './App.module.css';
 import { useFetchContactsQuery } from './redux/slice';
 import { FaRegAddressBook } from 'react-icons/fa';
-import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
-import { useThemeSwitcher } from 'react-css-theme-switcher';
+
 
 
 
 export default function App() {
   const { data, error, isFetching } = useFetchContactsQuery()
-  const themes = {
-    light: 'public/light.css',
-    dark: 'public/dark.css',
-  };
-  
+
 
 const contacts = useSelector(state =>  state.contacts)
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
 
   return (
-    <ThemeSwitcherProvider defaultTheme="light" themeMap={themes}>
+ 
     <Container>
       <h1 className={s.title}>Phone Book <FaRegAddressBook style={{ width: 25, height: 25, marginLeft: '10px'}}/></h1>
       <Form contacts={data}/>
@@ -38,7 +33,7 @@ const contacts = useSelector(state =>  state.contacts)
       <Filter />
       <ContactList data={data}/>
     </Container>
-    </ThemeSwitcherProvider>
+
   );
 }
 
